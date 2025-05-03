6344c55547530ea944d20de7e35456cffb6b7c9e
Bryan Suquillo <brsume@alumnatflorida.es>
2025-05-03 10:24:57 +0200

    Configuración del archivo principal index

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+app.set("port", process.env.PORT || 8080);
+app.set("json spaces", 2);
+
+//Middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+//Rutas
+app.use(require("./Routes/index"));
+
+app.listen(app.get("port"), () => {
+  console.log(`hola desde el puerto: ${app.get("port")}`);
+});
